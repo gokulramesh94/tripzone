27d7188c90ccc586bb0dec87d96b76cd2e393cdf
Gokul <61695419+gokulramesh94@users.noreply.github.com>
2021-07-22 16:50:40 -0400

    Update useWorldClock.js
    Fixed incorrect IST time bug.

--- src/hooks/useWorldClock.js
+++ src/hooks/useWorldClock.js
@@ -1,38 +1,40 @@
 import { useEffect, useState } from "react";
 import { getDateTime } from "../utils/timeUtils";
-import { getWorldClocks } from "../services";
+import { getIST, getWorldClocks } from "../services";
 
 function useWorldClock() {
   const [sessionTime, setSessionTime] = useState(0);
   const [offset, setOffset] = useState({});
-  const [worldTime,setWorldTime] = useState({});
-  
+  const [worldTime, setWorldTime] = useState({});
+
   const getDate = () => {
-    getWorldClocks()
-      .then(response => {
-        setOffset(response.utcOffset);
-        setWorldTime({est:getDateTime(response.currentDateTime, response.utcOffset), ist: getDateTime(response.currentDateTime)});
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    getIST().then((istResponse) => {
+      getWorldClocks()
+        .then((response) => {
+          setOffset(response.utcOffset);
+          setWorldTime({
+            est: getDateTime(response.currentDateTime, response.utcOffset),
+            ist: getDateTime(response.currentDateTime, istResponse.utc_offset),
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
   };
 
   useEffect(() => {
     getDate();
   }, []);
 
-  useEffect(
-    () => {
-      const timer = setTimeout(() => {
-        setSessionTime(sessionTime => sessionTime + 1);
-        let date = new Date();
-        setWorldTime({est:getDateTime(date, offset), ist: getDateTime(date)});
-      }, 1000 * 60);
-      return () => clearTimeout(timer);
-    },
-    [offset, sessionTime]
-  );
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSessionTime((sessionTime) => sessionTime + 1);
+      let date = new Date();
+      setWorldTime({ est: getDateTime(date, offset), ist: getDateTime(date) });
+    }, 1000 * 60);
+    return () => clearTimeout(timer);
+  }, [offset, sessionTime]);
   return [sessionTime, worldTime];
 }
 
