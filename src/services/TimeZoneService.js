cbaf098d6e995729928fe548d86b4c695659a801
Gokul <61695419+gokulramesh94@users.noreply.github.com>
2021-07-22 16:51:33 -0400

    Update TimeZoneService.js
    Added an API call to fetch IST offset.

--- src/services/TimeZoneService.js
+++ src/services/TimeZoneService.js
@@ -9,6 +9,15 @@ export const getWorldClocks = async () => {
     );
     return response.data;
   } catch (error) {
-    console.log("Error - loginService -> loginUser : ", error);
+    console.log("Error - TimeZoneService -> getWorldClocks : ", error);
+  }
+};
+
+export const getIST = async () => {
+  try {
+    const response = await appAxios.get(Strings.APPLICATION.END_POINTS.IST, {});
+    return response.data;
+  } catch (error) {
+    console.log("Error - TimeZoneService -> getIST : ", error);
   }
 };
